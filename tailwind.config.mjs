35120347c222a075ab4d7c67916efa180681ee73
D0NN1E <paradorn3237@gmail.com>
2023-12-19 11:58:05 +0700

    .

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,26 +1,8 @@
-import starlightPlugin from "@astrojs/starlight-tailwind";
-import animatePlugin from "tailwindcss-animate";
-import colors from "tailwindcss/colors";
-import defaultTheme from "tailwindcss/defaultTheme";
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      colors: {
-        // Your preferred accent color. Indigo is closest to Starlight’s defaults.
-        accent: colors.fuchsia,
-        // Your preferred gray scale. Zinc is closest to Starlight’s defaults.
-        gray: colors.zinc,
-      },
-      fontFamily: {
-        sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
-      },
-      screens: {
-        fifty: "50rem",
-      },
-    },
-  },
-  plugins: [starlightPlugin(), animatePlugin],
-};
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
