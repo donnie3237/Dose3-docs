ac628925e0bb886f062e0c9e18493ff366aa4968
D0NN1E <paradorn3237@gmail.com>
2023-12-19 12:20:22 +0700

    .

--- src/components/Licence.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  const year = new Date().getFullYear();
-  return (
-    <div class="flex justify-center self-center flex-col w-[100%]">
-        <hr class="bg-orange-700 w-[100%] h-[1px] mb-3"/>
-        <p>&copy; {year} DOSE FROM ANOTHER PLANET</p>
-    </div>
-  )
-}
